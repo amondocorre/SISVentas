4b1ba93021514b0836b8f9ddb8993bc9ef5f6e14
amondocorre <amondocorre@capressocafe.com>
2025-06-23 12:29:25 -0400

    ACTUALIZACION v1 composer

--- model/Conexion.php
+++ model/Conexion.php
@@ -1,5 +1,5 @@
 <?php
-	$conexion = new mysqli("localhost", "root", "", "ventas");
+	$conexion = new mysqli("localhost", "root", "", "solventas");
 	//$conexion = new mysqli("67.225.151.112", "vanguard_admin_hotel", "Hotel.hackers.", "vanguard_Tienda");
 	//$conexion = new mysqli("localhost", "vanguard_admin_hotel", "Hotel.hackers.", "vanguard_Tienda");
 	if (mysqli_connect_errno()) {
