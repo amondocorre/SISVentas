ea94f66305b176caba2a67eb65e4c6fb00bc067a
omartru10 <trujillo.0643@gmail.com>
2025-06-23 14:38:07 -0400

    .

--- public/scripts/Venta.js
+++ public/scripts/Venta.js
@@ -85,7 +85,7 @@ function init(){
                       }
                     });*/
 
-                    //location.reload();
+                    location.reload();
                 } else {
                     $("#btnNuevoPedido").show();
                     bootbox.prompt({
